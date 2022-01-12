e19401fdb3e14d6a2d9c5dbbb9a7bc39774daa4a
Tacio de Souza Campos <taciodesouzacampos@gmail.com>
2022-01-11 23:33:34 -0300

    first commit

---
+++ intercom-inbox-ui/index.ts
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // const simpleBarContent = document.querySelector('.center .simplebar-content');
+  // simpleBarContent.classList.add('flex', 'flex-col', 'h-full', 'justify-end');
+});
